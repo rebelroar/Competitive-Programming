e1523a0350eccc9409ee5ceddb55d8fbd5d10aaf
SHASHANK MISHRA <47750819+ReBelRoar@users.noreply.github.com>
2021-12-14 19:23:04 +0530

    Update segment_tree.cpp

--- Segment Tree/segment_tree.cpp
+++ Segment Tree/segment_tree.cpp
@@ -1,3 +1,6 @@
+
+// For finding minimum in the given range
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,15 +9,16 @@ void buildTree(vector<int> a, int s, int e, vector<int> &tree, int tree_ind) {
 		tree[tree_ind] = a[s];
 		return;
 	}
+
 	int mid = (s + e) >> 1;
-	buildTree(a, s, mid, tree, 2 * tree_ind);
-	buildTree(a, mid + 1, e, tree, 2 * tree_ind + 1);
-	tree[tree_ind] = min(tree[2 * tree_ind], tree[2 * tree_ind + 1]);
+	buildTree(a, s, mid, tree, 2 * tree_ind + 1);
+	buildTree(a, mid + 1, e, tree, 2 * tree_ind + 2);
+
+	tree[tree_ind] = min(tree[2 * tree_ind + 1], tree[2 * tree_ind + 2]);
 	return;
 }
 
 int query(vector<int> tree, int start, int end, int ss, int se, int tree_ind) {
-	// For finding minimum
 
 	// full overlap
 	if (start <= ss && end >= se) return tree[tree_ind];
@@ -23,8 +27,8 @@ int query(vector<int> tree, int start, int end, int ss, int se, int tree_ind) {
 	if (start > se || end < ss) return INT_MAX;
 
 	int mid = (ss + se) >> 1;
-	int left = query(tree, start, end, ss, mid, 2 * tree_ind);
-	int right = query(tree, start, end, mid + 1, se, 2 * tree_ind + 1);
+	int left = query(tree, start, end, ss, mid, 2 * tree_ind + 1);
+	int right = query(tree, start, end, mid + 1, se, 2 * tree_ind + 2);
 	return min(left, right);
 }
 
@@ -36,10 +40,12 @@ void updateNode(vector<int> &tree, int ss, int se, int tree_ind, int update_ind,
 		tree[tree_ind] += increment;
 		return;
 	}
+
 	int mid = (se + ss) >> 1;
-	updateNode(tree, ss, mid, 2 * tree_ind, update_ind, increment);
-	updateNode(tree, mid + 1, se, 2 * tree_ind + 1, update_ind, increment);
-	tree[tree_ind] = min(tree[tree_ind * 2], tree[2 * tree_ind + 1]);
+	updateNode(tree, ss, mid, 2 * tree_ind + 1, update_ind, increment);
+	updateNode(tree, mid + 1, se, 2 * tree_ind + 2, update_ind, increment);
+
+	tree[tree_ind] = min(tree[tree_ind * 2 + 1], tree[2 * tree_ind + 2]);
 	//return;
 }
 
@@ -52,27 +58,30 @@ void updateRange(vector<int> &tree, int ss, int se, int start, int end, int tree
 	}
 
 	int mid = (se + ss) >> 1;
-	updateRange(tree, ss, mid, start, end, 2 * tree_ind, increment);
-	updateRange(tree, mid + 1, se, start, end, 2 * tree_ind + 1, increment);
+	updateRange(tree, ss, mid, start, end, 2 * tree_ind + 1, increment);
+	updateRange(tree, mid + 1, se, start, end, 2 * tree_ind + 2, increment);
 
-	tree[tree_ind] = min(tree[2 * tree_ind], tree[2 * tree_ind + 1]);
+	tree[tree_ind] = min(tree[2 * tree_ind + 1], tree[2 * tree_ind + 2]);
 	return;
 }
 
 int main() {
 	int n, q, start, end;
 	cin >> n;
-	std::vector<int> a(n), tree(4 * n + 1);
+	std::vector<int> a(n), tree(4 * n);
 	for (int u = 0; u < n; u++)cin >> a[u];
-	buildTree(a, 0, n - 1, tree, 1);
+	buildTree(a, 0, n - 1, tree, 0);
 
 	// Increment all values from index 1 to 3 by +5
-	updateRange(tree, 0, n - 1, 1, 3, 1, +5);
+	updateRange(tree, 0, n - 1, 1, 3, 0, +5);
+
+	// Increment value at index 2 by +10
+	updateNode(tree, 0, n - 1, 0, 2, +10);
 
 	cin >> q;
 	while (q--) {
 		cin >> start >> end;
-		cout << query(tree, start, end, 0, n - 1, 1) << endl;
+		cout << query(tree, start, end, 0, n - 1, 0) << endl;
 	}
 	return 0;
 }
